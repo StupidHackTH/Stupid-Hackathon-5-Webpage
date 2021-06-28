4005629567f4200a397316503411606133212dac
bTch <betich365@gmail.com>
2021-06-28 20:12:23 +0700

    add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,13 @@ module.exports = {
     },
     extend: {
       colors: {
+        "stupid-blue": "#1b00ff",
         "stupid-green": "#06FF00",
         "stupid-yellow": "#EFE719",
-        "stupid-pink": "#EB1FFE",
+        "stupid-pink": "#ff41a6",
+        "stupid-violet": "#EB1FFE",
+        "stupid-red": "#ff2600",
+        "stupid-black": "#000"
       },
     },
   },
