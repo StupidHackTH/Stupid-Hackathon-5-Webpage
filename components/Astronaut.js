a6fc2e76cc0346ee64b92cbd688a1bd7195a7697
bTch <betich365@gmail.com>
2021-06-28 11:32:42 +0700

    link to local images

--- components/Astronaut.js
+++ components/Astronaut.js
@@ -1,39 +1,39 @@
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 
 export default (props) => {
-    useLayoutEffect(() => {
-      const container = document.getElementById('wrapper');
-  
-      const handleScroll = ({ target }) => {
-        const initY = 30;
-        const doc = document.documentElement;
-        const vh = Math.max(doc.clientHeight || 0, window.innerHeight || 0)
-        var y = (window.pageYOffset || target.scrollTop) - (doc.clientTop || 0);
-  
-        var astro = document.getElementById("astro");
-        var aPos, aScale, aRot;
-  
-        var dY = y - initY
-  
-        aPos = initY + (0.75 * dY)
-        aScale = (vh - (initY + ((0.3) * dY))) / 2 / vh * 100
-        aRot = 90 - (vh - (initY + ((0.3) * dY))) / 2 / vh * 180;
-  
-        if (aScale < 25) {
-          aScale = 25;
-          aRot = 90 - (vh - (initY + ((2) * dY))) / 2 / vh * 180;
-        }
-  
-        astro.style.top = `${aPos}px` * 0.75;
-        astro.style.height = `${aScale}%`
-        astro.style.transform = `rotate(${aRot}deg)`;
+  useEffect(() => {
+    const container = document.getElementById('wrapper');
+
+    const handleScroll = ({ target }) => {
+      const initY = 30;
+      const doc = document.documentElement;
+      const vh = Math.max(doc.clientHeight || 0, window.innerHeight || 0)
+      var y = (window.pageYOffset || target.scrollTop) - (doc.clientTop || 0);
+
+      var astro = document.getElementById("astro");
+      var aPos, aScale, aRot;
+
+      var dY = y - initY
+
+      aPos = initY + (0.75 * dY)
+      aScale = (vh - (initY + ((0.3) * dY))) / 2 / vh * 100
+      aRot = 90 - (vh - (initY + ((0.3) * dY))) / 2 / vh * 180;
+
+      if (aScale < 25) {
+        aScale = 25;
+        aRot = 90 - (vh - (initY + ((2) * dY))) / 2 / vh * 180;
       }
-  
-      container.addEventListener('scroll', handleScroll, false)
-      return () => container.removeEventListener('scroll', handleScroll)
-    }, [])
-  
-    return (
-      <img src="https://drive.google.com/uc?id=11Un8VFE2LsNPCS0bEW9D3N_J2UqFaQPJ" id="astro" alt="astronaut" />
-    );
-  }
+
+      astro.style.top = `${aPos}px` * 0.75;
+      astro.style.height = `${aScale}%`
+      astro.style.transform = `rotate(${aRot}deg)`;
+    }
+
+    container.addEventListener('scroll', handleScroll, false)
+    return () => container.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  return (
+    <img src="/astronaut.png" id="astro" alt="astronaut" />
+  );
+}
