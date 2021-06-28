0eb0991463b0fb42c378d9c3e181095402bf5866
bTch <betich365@gmail.com>
2021-06-28 11:21:21 +0700

    move astronaut to new component

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,11 @@
 import Head from "next/head";
-import { useLayoutEffect, useState } from "react";
-import Header from "../components/Header.js";
-import Registeration from "../components/Registration.js";
-import WhatIs from "../components/WhatIs.js";
-import QA from "../components/QA.js";
-import Sponsor from "../components/Sponsor.js";
-import ContactUs from "../components/ContactUs.js";
-
-function Astro() {
-  useLayoutEffect(() => {
-    const container = document.getElementById('wrapper');
-
-    const handleScroll = ({ target }) => {
-      const initY = 30;
-      const doc = document.documentElement;
-      const vh = Math.max(doc.clientHeight || 0, window.innerHeight || 0)
-      var y = (window.pageYOffset || target.scrollTop) - (doc.clientTop || 0);
-
-      var astro = document.getElementById("astro");
-      var aPos, aScale, aRot;
-
-      var dY = y - initY
-
-      aPos = initY + (0.75 * dY)
-      aScale = (vh - (initY + ((0.3) * dY))) / 2 / vh * 100
-      aRot = 90 - (vh - (initY + ((0.3) * dY))) / 2 / vh * 180;
-
-      if (aScale < 25) {
-        aScale = 25;
-        aRot = 90 - (vh - (initY + ((2) * dY))) / 2 / vh * 180;
-      }
-
-      astro.style.top = `${aPos}px` * 0.75;
-      astro.style.height = `${aScale}%`
-      astro.style.transform = `rotate(${aRot}deg)`;
-    }
-
-    container.addEventListener('scroll', handleScroll, false)
-    return () => container.removeEventListener('scroll', handleScroll)
-  }, [])
-
-  return (
-    <img src="https://drive.google.com/uc?id=11Un8VFE2LsNPCS0bEW9D3N_J2UqFaQPJ" id="astro" alt="astronaut" />
-  );
-}
+import Header from "../components/index/Header.js";
+import Registeration from "../components/index/Registration.js";
+import WhatIs from "../components/index/WhatIs.js";
+import QA from "../components/index/QA.js";
+import Sponsor from "../components/index/Sponsor.js";
+import ContactUs from "../components/index/ContactUs.js";
+import Astronaut from "../components/Astronaut.js"
 
 export default function Home() {
   return (
@@ -54,7 +16,7 @@ export default function Home() {
       >
         <Header />
 
-        <Astro />
+        <Astronaut />
 
         <Registeration />
 
