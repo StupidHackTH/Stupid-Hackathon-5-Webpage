67b865c9dc2fdeb3d02ace3b65feabb3431becae
bTch <betich365@gmail.com>
2021-06-29 16:02:08 +0700

    add thai font

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,13 @@ import QA from "../components/index/QA.js";
 import Sponsor from "../components/index/Sponsor.js";
 import ContactUs from "../components/index/ContactUs.js";
 
-// TODO generate boxes using for loop
-// TODO put box into a separate component
 // TODO Navbar
 
 export default function Home() {
   return (
     <>
       <main
-        className="wrapper wrapper__space  font-fasys"
+        className="wrapper wrapper__space"
       >
         <Header />
 
