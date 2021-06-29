1cf48a86abb33d9270b0ba2444d5382fd47bc01d
Chayapatr Archiwaranguprok <mail@chayapatr.com>
2021-06-29 20:03:52 +0700

    🖌 Edit styles

--- components/index/Header.js
+++ components/index/Header.js
@@ -5,16 +5,20 @@ const Header = (props) => (
   <>
     <header className="container section" id="header">
       <div id="heading-title">
-        <h1>
-          <p>The 5th</p>
-          <p>StuPId H@CK</p>
-          <p>THaIL@Nd</p>
-        </h1>
+        <p>The 5th</p>
+        <p>StuPId H@CK</p>
+        <p>THaIL@Nd</p>
       </div>
       <div className="btn-container">
-        <a href="#info" className="nes-btn nes-btn__green">M0re 1nFo</a>
-        <a href="#register" className="nes-btn nes-btn__green">Regi$teR</a>
-        <a href="#contact" className="nes-btn nes-btn__green">C0Nt4cT</a>
+        <a href="#info" className="nes-btn nes-btn__green">
+          M0re 1nFo
+        </a>
+        <a href="#register" className="nes-btn nes-btn__green">
+          Regi$teR
+        </a>
+        <a href="#contact" className="nes-btn nes-btn__green">
+          C0Nt4cT
+        </a>
       </div>
 
       <Astronaut />
