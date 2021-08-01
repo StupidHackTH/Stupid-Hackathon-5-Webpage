461da4bed849cc000fb9a2b7be02dcc7fba6367e
dtinth on Huawei Cloud ecs-b75d <dtinth@spacet.me>
2021-08-02 03:57:00 +0800

    Add ID for staff projects

--- src/helpers/staticData.ts
+++ src/helpers/staticData.ts
@@ -10,12 +10,13 @@ export const noLive = [
 
 export const staffTeams: Team[] = [
     {
-        id: 'staff1',
+        id: 'Staff01',
         color: 'd7fc70',
         name: 'dtinth',
         members: ['dtinth'],
         projects: [
             {
+                id: 'Staff01-0',
                 name: 'misheard',
                 description:
                     'โปรแกรมแชทหัวร้อน — A stupid chat application in which you could not type your message but must rely on speech-to-text to transcribe what you wish to convey.',
@@ -24,12 +25,13 @@ export const staffTeams: Team[] = [
         ]
     },
     {
-        id: 'staff2',
+        id: 'Staff02',
         color: '4daaa4',
         name: 'natthanat',
         members: ['natthanat'],
         projects: [
             {
+                id: 'Staff02-0',
                 name: 'TV Station Simulator',
                 description:
                     'A virtual world role play as TV Station operators  with AI watching for virtual rating.',
