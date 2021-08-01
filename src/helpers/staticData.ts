5ccf0be03d04c7accc5f05303553ef9c5639ecec
dtinth on Huawei Cloud ecs-b75d <dtinth@spacet.me>
2021-08-02 02:47:50 +0800

    Add more staff teams

--- src/helpers/staticData.ts
+++ src/helpers/staticData.ts
@@ -10,14 +10,29 @@ export const noLive = [
 export const staffTeams: Team[] = [
     {
         id: 'staff1',
-        color: '#d7fc70',
+        color: 'd7fc70',
         name: 'dtinth',
-        members: ['104986860236877824'],
+        members: ['dtinth'],
         projects: [
             {
                 name: 'misheard',
-                description: 'โปรแกรมแชทหัวร้อน',
+                description:
+                    'โปรแกรมแชทหัวร้อน — A stupid chat application in which you could not type your message but must rely on speech-to-text to transcribe what you wish to convey.',
                 link: 'https://youtu.be/2JE3DETHTQo'
+            }
+        ]
+    },
+    {
+        id: 'staff2',
+        color: '4daaa4',
+        name: 'natthanat',
+        members: ['natthanat'],
+        projects: [
+            {
+                name: 'TV Station Simulator',
+                description:
+                    'A virtual world role play as TV Station operators  with AI watching for virtual rating.',
+                link: 'https://www.youtube.com/watch?v=iSVvLDotKd4'
             }
         ]
     }
